9681528509e9a4727254a3231958051255c455a6
Fallenfromheaven <156500509+Fallenfromheaven@users.noreply.github.com>
2024-12-29 15:10:44 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,3 @@
+local source = loadstring(game:HttpGet("https://raw.githubusercontent.com/Fallenfromheaven/Scriptloader-V2/refs/heads/main/source.lua"))()
+queue_on_teleport(source)
+loadstring(source)()
