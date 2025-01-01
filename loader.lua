21ba65783df3490b9d7b55b458ad1d3d84b60601
Fallenfromheaven <156500509+Fallenfromheaven@users.noreply.github.com>
2025-01-01 18:24:48 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Fallenfromheaven/Scriptloader-V2/refs/heads/main/source.lua"))()
+queue_on_teleport("task.wait(20)loadstring(game:HttpGet('https://raw.githubusercontent.com/Fallenfromheaven/Scriptloader-V2/refs/heads/main/loader.lua'))()")
