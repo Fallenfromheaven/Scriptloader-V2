80f279c2421a3aea43fd7c3d1585e4beb594eca9
Fallenfromheaven <156500509+Fallenfromheaven@users.noreply.github.com>
2025-01-05 23:52:53 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Fallenfromheaven/Scriptloader-V2/refs/heads/main/source.lua"))()
-queue_on_teleport("task.wait(20)loadstring(game:HttpGet('https://raw.githubusercontent.com/Fallenfromheaven/Scriptloader-V2/refs/heads/main/loader.lua'))()")
+--queue_on_teleport("task.wait(20)loadstring(game:HttpGet('https://raw.githubusercontent.com/Fallenfromheaven/Scriptloader-V2/refs/heads/main/loader.lua'))()")
 -- print("hi uhh momentan disabled weil update :)")
