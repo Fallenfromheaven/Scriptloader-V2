17537a190d3429adad75dc8d3a52d491126d57d0
Fallenfromheaven <156500509+Fallenfromheaven@users.noreply.github.com>
2025-01-07 19:59:30 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -146,7 +146,6 @@ local u_Button_5 = Universal:CreateButton({
         RepoURL = "https://raw.githubusercontent.com/luau/SynSaveInstance/main/",
         SSI = "saveinstance",
         }
-        local synsaveinstance = loadstring(game:HttpGet(Params.RepoURL .. Params.SSI .. ".luau", true), Params.SSI)()
         local Options = {} -- Documentation here https://luau.github.io/UniversalSynSaveInstance/api/SynSaveInstance
         synsaveinstance(Options)
     end
