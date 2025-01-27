6759e3884f8f7be1b376218b17ae003e68f3d8ab
Fallenfromheaven <156500509+Fallenfromheaven@users.noreply.github.com>
2025-01-28 00:45:25 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -26,7 +26,8 @@ local gamePlaceIDs = {
     ["Slab Battles"]     = {6403373529}, -- maybe missing place ids idk
     ["Combat Warriors"]  = {4282985734, 11979315221, 9532476417, 16323991061, 18480127448, 18480122340},
     ["KAT"]              = {621129760},
-    ["TSB"]              = {10449761463, 131048399685555, 130818724007978, 12360882630, 10449761463}
+    ["TSB"]              = {10449761463, 131048399685555, 130818724007978, 12360882630, 10449761463},
+    ["Da Hood"]          = {2788229376, 7213786345}
 }
 
 local function resolveGameNameByPlaceID(placeId)
@@ -535,6 +536,65 @@ if game_name == "TSB" then
         end
         })
     local g12_Divider_3 = game12:CreateDivider()
+end
+
+if game_name == "Da Hood" then
+
+    local game13 = Window:CreateTab("Da Hood", 4483362458)
+
+    local g13_Section_1 = game13:CreateSection("Stand Script")
+    local g13_Input_1 = game13:CreateInput({
+        Name = "Stand Owner",
+        CurrentValue = "",
+        PlaceholderText = "Input Placeholder",
+        RemoveTextAfterFocusLost = false,
+        Flag = "g13_input_1",
+        Callback = function(Text)
+            getgenv().owner = Rayfield.Flags["g13_input_1"].CurrentValue
+        end,
+    })
+    local g13_Button_1 = game13:CreateButton({
+        Name = "Stand Script",
+        Callback = function()
+            getgenv()._="Join discord.gg/msgabv2t9Q | discord.gg/stando to get latest update ok bai aaa| If you pay for this script you get scammed, this script is completely free ok"
+            getgenv().Owner = getgenv().owner
+            getgenv().Configuration = {
+                ['Msg'] = 'Yare Yare Daze.', --When You Say [/e msg] It Makes All The Alts Say That Message
+                ['CrewID'] = 32570691, --Must join group
+                ['Attack'] = 'Heavy', --[Heavy | Light] Heavy = Super Punch Light = Spam Punch
+                ['TPMode'] = 'Cart', --[Knock | Cart] --Teleport mode
+                ['GunMode'] = 'Aug', --[LMG | Rifle | Aug] --Self explain
+                ['FlyMode'] = 'Heaven', --[Glide | FlyV1 | FlyV2 | Hover | Heaven ]
+                ['Position'] = 'Back', --[Back | Left | Right | Mid | UpMid | UpLeft | UpLeft | Walk ]
+                ['Resolver'] = false, --[true | false] Turn On Or Off Resolver If Someone Using Anti Lock [This only applied to attack only for guns it's already have resolver]
+                ['ChatCmds'] = true, --[true | false] if true stand talks to you
+                ['AutoMask'] = true, --[true | false] Auto Buy Mask When The Stand Respawn
+                ['MaskMode'] = 'Riot', --[Surgeon | Paintball | Skull | Pumpkin | Hockey | Ninja | Riot | Breathing] The Stand Will Buy The Mask You Selected
+                ['AntiStomp'] = true, --[true | false] The Stand Will Cannot Be Stomped Or Grabbed
+                ['StandMode'] = 'Star Platinum : OverHeaven', --[Star Platinum : OverHeaven | Star Platinum: The World | Star Platinum, Za Warudo! | TheWorld | Cmoon | King Crimson | Killer Queen | MIH | D4C]
+                ['Smoothing'] = false, --[true | false] if true stand will follow you smoothly
+                ['AttackMode'] = 'Sky', --[Sky | Under] If [Sky] The Stand Will Attack Infront Of Target And If [Under] The Stand Will Attack In Underground
+                ['CustomName'] = 'Master', --Custom Name Text Default Is [Master] Example Of it 'Im Here, Master'
+                ['Hidescreen'] = false, --[true | false] If you set it to true you will see white blank screen [Save up gpu usage]
+                ['SummonPoses'] = 'Pose3', --[false | Pose1 | Pose2 | Pose3]
+                ['LowGraphics'] = false, --[true | false] Turns graphics down so your stand doesnt lag too much
+                ['CustomPrefix'] = '.', --[ ! | / | . | ? | > | < | $ | ; | : ] Custom target prefix
+                ['CustomSummon'] = 'Summon!', --Custom [Summon!] Text
+                ['AutoPrediction'] = false, --[true | false] Turn On Or Off Auto Prediction 
+                ['AttackDistance'] = 75, --Attack distance for "Attack!, Stab!, Gkill!" max distance
+                ['AutoSaveLocation'] = 'Downhill_Rooftop', --[Downhill_Rooftop | Uphill_Rooftop | DA_FURNITURE ]
+                ['AttackPrediction'] = 0.34, --Prediction For Attack Or Targetting
+                ['AttackAutoPrediction'] = 0.23, --Prediction For Attack Or Targetting
+                --//    ! Sounds !     \--
+                --\ MUST HAVE BOOMBOX //--
+                ['Sounds'] = true, --[true | false]
+                ['CustomSong'] = 123456, --Enter A Song ID And Say 'Song!'
+                ['SummonMusic'] = true, --[true | false]
+                ['SummonMusicID'] = 'Default' -- Enter a Sound ID Put [Default] If You Would Like The Default Sound
+            }loadstring(game:HttpGet("https://xk5ng.github.io/V4.8"))()
+        end
+        })
+    local g13_Divider_1 = game13:CreateDivider()
 end
 
 local settings_ = Window:CreateTab("Settings", 4483362458)
